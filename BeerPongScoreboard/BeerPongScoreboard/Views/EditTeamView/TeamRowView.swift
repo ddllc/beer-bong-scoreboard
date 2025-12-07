c2e0696e8ca1814a9d62a4d4fc82ed88734842fb
ddllc <donald_mallow@icloud.com>
2025-12-07 17:26:34 -0500

    style-editteamsrowview

--- BeerPongScoreboard/BeerPongScoreboard/Views/EditTeamView/TeamRowView.swift
+++ BeerPongScoreboard/BeerPongScoreboard/Views/EditTeamView/TeamRowView.swift
@@ -2,30 +2,98 @@ import SwiftUI
 
 struct EditTeamsRowView: View {
     let team: TeamEntity
+    private let avatarSize: CGFloat = 75
 
     var body: some View {
         HStack {
-            
-            if let teamPhoto = team.photoData {
-                Image(uiImage: UIImage(data: teamPhoto)!)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
+                // MARK: - Team Column
+            VStack {
+                    ZStack {
+                        Circle()
+                            .fill(Color(.systemGray5))
 
-            } else {
-                Image(systemName: "photo.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
+                        if let data = team.photoData,
+                           let uiImage = UIImage(data: data) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                        } else {
+                            Image(systemName: "person.2.fill")
+                                .font(.system(size: avatarSize * 0.4))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    .frame(width: avatarSize, height: avatarSize)
+                    .clipShape(Circle())
 
+                    Text(team.name)
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200)
+
+                Spacer()
+                }
+
+            Divider()
+                .frame(maxHeight: .infinity)
+                .background(Color(.white).fontWeight(.heavy))
+
+                // MARK: - Players Columns
+            VStack(alignment: .leading) {
+                HStack {
+                    ZStack {
+                        Circle()
+                            .fill(Color(.systemGray5))
+
+                        if let data = team.players[0].photoData,
+                           let uiImage = UIImage(data: data) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                        } else {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: avatarSize * 0.4))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    .frame(width: avatarSize, height: avatarSize)
+                    .clipShape(Circle())
+
+                    Text(team.players[0].name)
+                }
+
+
+                HStack {
+                    ZStack {
+                        Circle()
+                            .fill(Color(.systemGray5))
+
+                        if let data = team.players[1].photoData,
+                           let uiImage = UIImage(data: data) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .scaledToFill()
+                        } else {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: avatarSize * 0.4))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    .frame(width: avatarSize, height: avatarSize)
+                    .clipShape(Circle())
+
+                    Text(team.players[1].name)
+                }
             }
 
-            Text(team.name)
-
-            Text(team.players[0].name)
-
-            Text(team.players[1].name)
-
-        }
+            Spacer()
+                // MARK: - Stats Column
+            VStack(alignment: .trailing) {
+                Text("Wins: \(team.wins)")
+                Text("Losses: \(team.losses)")
+            }
+            .font(.title3)
+            .bold()
+            }
     }
 }
