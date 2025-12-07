aea62a63861b667ecbc396a96c59b7cf18af71ed
ddllc <donald_mallow@icloud.com>
2025-12-07 17:37:22 -0500

    style-editteamsrowview

--- BeerPongScoreboard/BeerPongScoreboard/Views/EditTeamView/TeamRowView.swift
+++ BeerPongScoreboard/BeerPongScoreboard/Views/EditTeamView/TeamRowView.swift
@@ -2,8 +2,8 @@ import SwiftUI
 
 struct EditTeamsRowView: View {
     let team: TeamEntity
-    private let avatarSize: CGFloat = 75
-
+    private let teamAvatarSize: CGFloat = 110
+        private let playerAvatarSize: CGFloat = 65
     var body: some View {
         HStack {
                 // MARK: - Team Column
@@ -19,11 +19,11 @@ struct EditTeamsRowView: View {
                                 .scaledToFill()
                         } else {
                             Image(systemName: "person.2.fill")
-                                .font(.system(size: avatarSize * 0.4))
+                                .font(.system(size: teamAvatarSize * 0.4))
                                 .foregroundStyle(.secondary)
                         }
                     }
-                    .frame(width: avatarSize, height: avatarSize)
+                    .frame(width: teamAvatarSize, height: teamAvatarSize)
                     .clipShape(Circle())
 
                     Text(team.name)
@@ -52,11 +52,11 @@ struct EditTeamsRowView: View {
                                 .scaledToFill()
                         } else {
                             Image(systemName: "person.fill")
-                                .font(.system(size: avatarSize * 0.4))
+                                .font(.system(size: playerAvatarSize * 0.4))
                                 .foregroundStyle(.secondary)
                         }
                     }
-                    .frame(width: avatarSize, height: avatarSize)
+                    .frame(width: playerAvatarSize, height: playerAvatarSize)
                     .clipShape(Circle())
 
                     Text(team.players[0].name)
@@ -75,11 +75,11 @@ struct EditTeamsRowView: View {
                                 .scaledToFill()
                         } else {
                             Image(systemName: "person.fill")
-                                .font(.system(size: avatarSize * 0.4))
+                                .font(.system(size: playerAvatarSize * 0.4))
                                 .foregroundStyle(.secondary)
                         }
                     }
-                    .frame(width: avatarSize, height: avatarSize)
+                    .frame(width: playerAvatarSize, height: playerAvatarSize)
                     .clipShape(Circle())
 
                     Text(team.players[1].name)
